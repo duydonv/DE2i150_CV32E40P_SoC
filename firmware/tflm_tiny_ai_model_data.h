6c843bece8463c09fa6ca690de8eff003248051f
duydonv <phamduydong1000@gmail.com>
2026-06-05 11:20:24 +0700

    firmware: add TFLM tiny MLP ref-vs-opt benchmark

---
+++ firmware/tflm_tiny_ai_model_data.h
@@ -0,0 +1,7 @@
+#ifndef TFLM_TINY_AI_MODEL_DATA_H
+#define TFLM_TINY_AI_MODEL_DATA_H
+
+constexpr unsigned int g_tflm_tiny_ai_model_data_size = 2288;
+extern const unsigned char g_tflm_tiny_ai_model_data[];
+
+#endif
